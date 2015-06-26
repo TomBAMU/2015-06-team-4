e077dff265b1ca97fcc6490054e9f7f4121e1675
superbort <a.broemmer@gmail.com>
2015-06-26 12:58:38 +0100

    Tests for Task 6

--- test/client/ui/NewMovie.js
+++ test/client/ui/NewMovie.js
@@ -3,18 +3,21 @@ module.exports = function() {
 
     this.title = element(by.model('movie.title'));
     this.description = element(by.model('movie.description'));
+    this.year = element(by.model('movie.year'));
     this.save = element(by.css('.btn-primary'));
 
     this.open = function() {
         browser.get('/movies/new');
     };
 
-    this.addMovie = function(title, description) {
+    this.addMovie = function(title, description, year) {
         this.open();
         this.title.clear();
         this.title.sendKeys(title);
         this.description.clear();
         this.description.sendKeys(description);
+        this.year.clear();
+        this.year.sendKeys(year);
         this.save.click();
     };
 };
